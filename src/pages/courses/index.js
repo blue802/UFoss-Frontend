dcfe54959d27fb1497bbe85c0be0db2d5366944e
Thiet <thiettruong1999@gmail.com>
2021-06-18 17:59:17 +0700

    save code

--- src/pages/courses/index.js
+++ src/pages/courses/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Stack, Flex, Box, Heading, Text, AspectRatio, Button } from '@chakra-ui/react';
+import { Stack, Flex, Box, Heading, Text, AspectRatio, Button, Spacer } from '@chakra-ui/react';
 import { useMediaQuery } from '@chakra-ui/media-query';
-import { BsFillStarFill } from 'react-icons/bs'
+import { BsFillStarFill } from 'react-icons/bs';
+import { AiOutlinePlaySquare } from 'react-icons/ai';
+import { FiPlayCircle } from 'react-icons/fi';
+
 function Courses(props) {
   const { course } = props;
   Courses.propTypes = {
@@ -58,13 +61,70 @@ function Courses(props) {
       </Flex >
       <Flex>
         <Box w='70%' margin='auto' >
-          <Flex>
-            <Box w='65%' bg='red' >
-              <Text>123</Text>
+          <Flex >
+            <Box w='65%' borderWidth='1px' borderColor='#DCDACB'>
+              <Flex direction='column'>
+                <Box borderWidth='1px' borderColor='#DCDACB'>
+                  <Flex direction='column'>
+                    {/* This is lessons */}
+                    <Box borderWidth='1px' borderColor='#DCDACB' padding='10px' cursor='pointer' cursor='pointer'>
+                      <Flex>
+                        <Box paddingTop='3px' paddingRight='10px'>
+                          <FiPlayCircle />
+                        </Box>
+                        <Text fontSize='16px' color='#3c3b37' fontWeight='900'>Auto-Welcome Message</Text>
+                        <Spacer />
+                        <Box>
+                          <Flex>
+                            <Text marginRight="10px">Preview</Text>
+                            <Text>5: 33</Text>
+                          </Flex>
+                        </Box>
+                      </Flex>
+                    </Box>
+                    <Box borderWidth='1px' borderColor='#DCDACB' padding='10px' cursor='pointer' >
+                      <Flex>
+                        <Box paddingTop='3px' paddingRight='10px'>
+                          <FiPlayCircle />
+                        </Box>
+                        <Text fontSize='16px' color='#3c3b37' fontWeight='900'>Auto-Welcome Message</Text>
+                        <Spacer />
+                        <Box>
+                          <Flex>
+                            <Text marginRight="10px">Preview</Text>
+                            <Text>5: 33</Text>
+                          </Flex>
+                        </Box>
+                      </Flex>
+                    </Box>
+                    <Box borderWidth='1px' borderColor='#DCDACB' padding='10px' cursor='pointer' >
+                      <Flex>
+                        <Box paddingTop='3px' paddingRight='10px'>
+                          <FiPlayCircle />
+                        </Box>
+                        <Text fontSize='16px' color='#3c3b37' fontWeight='900'>Auto-Welcome Message</Text>
+                        <Spacer />
+                        <Box>
+                          <Flex>
+                            <Text marginRight="10px">Preview</Text>
+                            <Text>5: 33</Text>
+                          </Flex>
+                        </Box>
+                      </Flex>
+                    </Box>
+                  </Flex>
+                </Box>
+              </Flex>
+            </Box>
+            <Box>
+              <Text>
+                123
+              </Text>
             </Box>
             <Box width='35%' justifyContent='center'>
+
               <Flex direction='column'>
-                <Box margin="auto">
+                <Box margin="auto" boxShadow="rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;">
                   <AspectRatio width='390px' height='192px' ratio={1}>
                     <iframe
                       title='naruto'
@@ -72,20 +132,53 @@ function Courses(props) {
                       allowFullScreen
                     />
                   </AspectRatio>
-                </Box>
-                <Box>
-                  <Text color='#3c3b37' fontSize='32px'>
-                    $13.99
-                  </Text>
-                  <Button>
-                    Add to cart
-                  </Button>
+                  <Box padding='24px'>
+                    <Text color='#3c3b37' fontSize='32px' fontWeight='700'>
+                      $13.99
+                    </Text>
+                    <Button w='full' fontWeight='800' bg='#EC5252' textColor='#fff' fontSize='16px' padding='25px 0px' marginBottom='10px'>
+                      Add to cart
+                    </Button>
+                    <Button w='full' fontWeight='800' borderColor='#0F7C90' borderWidth='1px' bg='#fff' textColor='#0F7C90' fontSize='16px' padding='25px 0px' marginBottom='25px'>
+                      Buy now
+                    </Button>
+                    <Text fontSize='12px' textAlign='center' >30-Day Money-Back Guarantee</Text>
+                    <Box>
+                      <Text marginBottom='8px' color='#3c3b37' fontWeight='900'>This course include:</Text>
+                      <Flex direction='column'>
+                        <Box padding='4px 0'>
+                          <Flex >
+                            <Box paddingTop='2px'><AiOutlinePlaySquare /></Box>
+                            <Text marginLeft='10px' fontSize='14px' color='#3c3b37'>22 hours on-demand video</Text>
+                          </Flex>
+                        </Box>
+                        <Box padding='4px 0'>
+                          <Flex >
+                            <Box paddingTop='2px'><AiOutlinePlaySquare /></Box>
+                            <Text marginLeft='10px' fontSize='14px' color='#3c3b37'>22 hours on-demand video</Text>
+                          </Flex>
+                        </Box>
+                        <Box padding='4px 0'>
+                          <Flex >
+                            <Box paddingTop='2px'><AiOutlinePlaySquare /></Box>
+                            <Text marginLeft='10px' fontSize='14px' color='#3c3b37'>22 hours on-demand video</Text>
+                          </Flex>
+                        </Box>
+                        <Box padding='4px 0'>
+                          <Flex >
+                            <Box paddingTop='2px'><AiOutlinePlaySquare /></Box>
+                            <Text marginLeft='10px' fontSize='14px' color='#3c3b37'>22 hours on-demand video</Text>
+                          </Flex>
+                        </Box>
+                      </Flex>
+                    </Box>
+                  </Box>
                 </Box>
               </Flex>
             </Box>
           </Flex>
         </Box>
-      </Flex>
+      </Flex >
     </Stack >
   )
 }
