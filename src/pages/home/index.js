3ef44d97080c4faf9764eb1b23081271a50a22ea
baobao <bluedev82@gmail.com>
2021-06-21 00:00:17 +0700

    create authentication service and implement login by google token

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,7 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import {
+  Box,
+  Heading,
+  Image,
+  ListItem,
+  OrderedList,
+  Text,
+} from '@chakra-ui/react';
+import axios from 'axios';
 
 const HomePage = () => {
-  return <div>Home Page</div>;
+  const [courses, setCourses] = useState([]);
+
+  useEffect(() => {
+    function fetchData() {
+      axios
+        .get('api/courses')
+        .then(res => res.data)
+        .then(data => setCourses(data));
+    }
+
+    fetchData();
+  }, []);
+
+  return (
+    <Box>
+      <OrderedList>
+        {courses.length > 0 ? (
+          courses.map(({ id, title, description, imageUrl }) => (
+            <ListItem key={id}>
+              <Image src={imageUrl} />
+              <Heading>{title}</Heading>
+              <Text>{description}</Text>
+            </ListItem>
+          ))
+        ) : (
+          <Text>Loading...</Text>
+        )}
+      </OrderedList>
+    </Box>
+  );
 };
 
 export default HomePage;
---
+++ src/services/auth.service.js
@@ -0,0 +1,163 @@
+/* eslint-disable import/no-anonymous-default-export */
+import { useEffect, useState } from 'react';
+import API from '../utils/API';
+
+const createTokenProvider = () => {
+  let _token = localStorage.getItem('TOKEN_AUTH');
+  _token = _token ? JSON.parse(_token) : null;
+  let listeners = [];
+
+  const getExpirationDate = jwtToken => {
+    if (!jwtToken) {
+      return null;
+    }
+    const jwt = JSON.parse(atob(jwtToken.split('.')[1]));
+
+    return (jwt && jwt.exp && jwt.exp * 1000) || null;
+  };
+
+  const isExpired = exp => {
+    if (!exp) {
+      return null;
+    }
+
+    return Date.now() > exp;
+  };
+
+  const getToken = async () => {
+    if (!_token) {
+      return null;
+    }
+
+    if (isExpired(getExpirationDate(_token.accessToken))) {
+      const updatedToken = await API.post(
+        '/update-token',
+        {},
+        {
+          headers: {
+            Authorization: `Basic ${_token.accessToken}`,
+          },
+        }
+      );
+
+      setToken(updatedToken);
+    }
+
+    return _token && _token.accessToken;
+  };
+
+  const loggedIn = () => {
+    if (!_token) {
+      return null;
+    }
+
+    const jwt = JSON.parse(atob(_token.tokenAccess.split('.')[1]));
+    const userInfo =
+      (jwt && {
+        username: jwt.name,
+        email: jwt.email,
+        avatarUrl: jwt.picture,
+      }) ||
+      null;
+
+    return userInfo;
+  };
+
+  const subscribe = listener => {
+    listeners.push(listener);
+  };
+
+  const unsubscribe = listener => {
+    listeners = listeners.filter(l => l !== listener);
+  };
+
+  const notify = () => {
+    const isLogged = loggedIn();
+    listeners.forEach(listener => listener(isLogged));
+  };
+
+  const setToken = token => {
+    if (token) {
+      localStorage.setItem('TOKEN_AUTH', JSON.stringify(token));
+    } else {
+      localStorage.removeItem('TOKEN_AUTH');
+    }
+    _token = token;
+    notify();
+  };
+
+  return {
+    getToken,
+    loggedIn,
+    setToken,
+    subscribe,
+    unsubscribe,
+  };
+};
+
+export const createAuthProvider = () => {
+  const tokenProvider = createTokenProvider();
+
+  const register = (username, email, password) => {
+    return API.post('/register', {
+      username,
+      email,
+      password,
+    });
+  };
+
+  const login = (username, password) => {
+    return API.post('/login', {
+      username,
+      password,
+    }).then(res => {
+      if (res.data) {
+        tokenProvider.setToken(res.data);
+      }
+      return res.data;
+    });
+  };
+
+  const logout = () => {
+    tokenProvider.setToken(null);
+  };
+
+  const authHeader = async () => {
+    const token = await tokenProvider.getToken();
+
+    return token
+      ? {
+          Authorization: `Basic ${token}`,
+        }
+      : {};
+  };
+
+  const useAuth = () => {
+    const [logged, setLogged] = useState(tokenProvider.loggedIn());
+
+    useEffect(() => {
+      const listener = newLogged => {
+        setLogged(newLogged);
+      };
+
+      tokenProvider.subscribe(listener);
+      return () => {
+        tokenProvider.unsubscribe(listener);
+      };
+    }, []);
+
+    return [logged];
+  };
+
+  return {
+    useAuth,
+    authHeader,
+    register,
+    login,
+    logout,
+    tokenProvider,
+  };
+};
+
+export const { login, register, logout, useAuth, authHeader, tokenProvider } =
+  createAuthProvider();
