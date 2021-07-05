acba1c9b137ab49bb617635fdd41260edccf1701
baobao <bluedev82@gmail.com>
2021-07-05 16:49:43 +0700

    change slides to show

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -40,8 +40,10 @@ function HomePage() {
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToShow: 2.5,
+          slidesToScroll: 2,
+          prevArrow: false,
+          nextArrow: false,
         },
       },
       {
@@ -92,7 +94,7 @@ function HomePage() {
           height={['25vh', 'auto']}
         />
       </Box>
-      {content}
+      <Box p={['1', '3', '5']}>{content}</Box>
     </Container>
   );
 }
